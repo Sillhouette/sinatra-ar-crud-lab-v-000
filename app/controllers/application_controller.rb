31a7d5d339e324cfbe70b5072a832d134084a87f
Richard Melchior <raustin.melchior@gmail.com>
2018-06-22 22:26:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,26 +13,26 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/posts' do
-    @post = Post.create(params) #Displays all of the posts
+    @post = Post.create(params) #Creates a post object with the params from new and redirects to /posts to display
     redirect to '/posts'
   end
 
-  get '/posts' do
+  get '/posts' do # This displays all of the posts
     @posts = Post.all
     erb :index
   end
 
-  get '/posts/:id' do
+  get '/posts/:id' do #This pulls a specific post based on the id number in the last part of the url
     @post = Post.find_by_id(params[:id])
     erb :show
   end
 
-  get '/posts/:id/edit' do
+  get '/posts/:id/edit' do #this gives the user an edit form for a specific post they want to edit
     @post = Post.find_by_id(params[:id])
     erb :edit
   end
 
-  patch '/posts/:id' do
+  patch '/posts/:id' do #this updates a specific post based on the post id then displays the post
     @post = Post.find_by_id(params[:id])
     @post.name = params[:name]
     @post.content = params[:content]
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
   end
 
 
-  delete '/posts/:id/delete' do
+  delete '/posts/:id/delete' do #this deletes a post
     @post = Post.find_by_id(params[:id])
     @post.delete
     erb :deleted
